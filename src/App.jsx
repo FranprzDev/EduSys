7619049a12b49fbcf1272e50d5aada645822b543
FranprzDev <franutn23@gmail.com>
2023-07-21 18:21:06 -0300

    Mergeo de las ramas error, popovers-admin, footer, contactanos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Navbar from './components/Navbar.jsx'
-import Error from './pages/Error.jsx'
 import Footer from './components/Footer'
+// También dejo comentada las rutas que no se utilizarán
+// import Error from './pages/Error.jsx'
+// import Contactanos from './components/Contactanos.jsx'
 
 function App() {
 
   return (
     <>
+    {/* Ojo aquí dejo comentado las cosas para probarlas, pero además de eso aún falta utilziar REACT-ROUTER para poder dejarlo bien */}
       <Navbar/>    
-    
+      {/* <Contactanos/> */}
+      {/* <Error/> */}
       <Footer/>
     </>
     

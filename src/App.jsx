a0efd6ed76964ec0a46192d69efd1ce9b7197b89
Eduardo Trigo <eduatrigo@gmail.com>
2023-07-21 23:48:21 -0300

    agregar componente Administracion y Modal-actualizarDatos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer'
+import Administracion from './components/Administracion.jsx'
 // También dejo comentada las rutas que no se utilizarán
 // import Error from './pages/Error.jsx'
 // import Contactanos from './components/Contactanos.jsx'
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
     {/* Ojo aquí dejo comentado las cosas para probarlas, pero además de eso aún falta utilziar REACT-ROUTER para poder dejarlo bien */}
-      <Navbar/>    
+      <Navbar/>   
+      <Administracion/> 
       {/* <Contactanos/> */}
       {/* <Error/> */}
       <Footer/>

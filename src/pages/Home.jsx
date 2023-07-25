8aa54482b498f9f857f9cbfc6bf519a9db20cc5d
Gise Delgado <giseladelgado909@gmail.com>
2023-07-25 20:49:13 -0300

    Creacóon del navBar de administración

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Contactanos from '../components/Contactanos'
 import InicioHome from '../components/InicioHome'
+import NavBarAdministracion from '../components/NavBarAdministracion'
 
 function Home() {
   return (
     <>
+    {/* <NavBarAdministracion/> */}
      <InicioHome/>
      <Contactanos/>
     </>

d82226cc3c6b489ccb7c6e63563a35178211e2fb
Fabricio Liquin <fabricio.liquin@gmail.com>
2023-07-25 18:24:19 -0300

    inicio del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PopoverInfo from './Popovers/PopoverInfo'
 import PopoverCuenta from './Popovers/PopoverCuenta'
 import '../styles/globalStyle.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function Navbar() {
   // {
@@ -12,9 +13,13 @@ function Navbar() {
 
   return (
     <>
-      <div>Navbar</div>
-      <PopoverCuenta/>
-      <PopoverInfo/>
+      <div>
+        <img src="../src/assets/Images/Logo.png" alt="Logo" style={{ height: "100px", width: "150px" }} />
+        <button className=''></button>
+        <UilInfoCircle size="40" color="#C9B7C7" />
+      </div>
+      <PopoverCuenta />
+      <PopoverInfo />
     </>
   )
 }

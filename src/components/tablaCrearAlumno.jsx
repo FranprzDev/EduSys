119c77c4039f5fda108a157c6791011f5e504d78
Eduardo Trigo <eduatrigo@gmail.com>
2023-07-26 16:46:53 -0300

    agregando componente tabla-alumnos

--- src/components/tablaCrearAlumno.jsx
+++ src/components/tablaCrearAlumno.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { UilFolderOpen, UilUser, UilTrashAlt } from '@iconscout/react-unicons'
 
 function TablaCrearAlumno() {
   return (
     <>
+    <div className="container table-responsive">
+    <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">Apellido</th>
+      <th scope="col">Año de Cursado</th>
+      <th scope="col">Cuota</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr >
+      <td className='text-white'style={{ background: "#866A80 " }}></td>
+      <td className='text-white'style={{ background: "#866A80 " }}></td>
+      <td className='text-white'style={{ background: "#866A80 " }}></td>
+      <td className='text-white'style={{ background: "#866A80 " }}></td>
+      <td className='text-white text-center'style={{ background: "#866A80 " }}><UilFolderOpen/><UilUser className="mx-3"/><UilTrashAlt/></td>
+    </tr>
+    
+  </tbody>
+</table>
+    </div>
     </>
   )
 }

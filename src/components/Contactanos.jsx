95fd42bd95fa4ba5742a9b8ae0c91f83221f1913
Gise Delgado <giseladelgado909@gmail.com>
2023-08-04 21:59:37 -0300

    Creacion de emailjs

--- src/components/Contactanos.jsx
+++ src/components/Contactanos.jsx
@@ -2,6 +2,39 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import ContactanosModal from "./modals/ContactanosModal";
 import { UilArrowRight } from "@iconscout/react-unicons";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+// emailjs
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_auaswc6', 'template_dbt5j6s', form.current, 'Nd0jlja_Fdxet6k68')
+      .then((result) => {
+          console.log(result.text); //mostrar mensaje de enviado
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+//formulario emailjs,
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+//--------------------------------------------------
 
 function Contactanos() {
   const [show, setShow] = useState(false);

9e74e503b3671cf459de685c08aa993b097cebe4
TaniaSantucho <tssantucho@gmail.com>
2023-08-08 03:46:26 -0300

    agrego modif

--- src/components/SomosNosotros.jsx
+++ src/components/SomosNosotros.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Conejo from './assets/images/Admin-Conejo.png'
+import Conejo from '../assets/images/Admin-Conejo.png'
 import { UilArrowRight } from '@iconscout/react-unicons'
-import { FaBeer } from "react-icons/fa";
 
-function InicioHome() {
+function SomosNosotros() {
     return (
         <>
             <div className="container row mx-auto py-lg-5 flex-md-row-reverse " >
@@ -28,12 +27,11 @@ function InicioHome() {
                 </div>
                 <div className="col-lg-6 mx-auto">
                     <div >
-                        <h1 className='text-center' style={{ color: "#866a80" }}>NombreDesarrollador</h1>
-                        <p className='text-center col-6 mx-auto my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta quod cum eos molestiae, eligendi est eveniet odio tenetur odit dolor asperiores totam voluptatibus qui. Exercitationem, dolorem. Inventore, dolorem doloribus!</p>
-                        <h3 className='text-center' style={{ color: "#866a80" }}>Perfil de Linkedin <FaBeer />?</h3>
-                        <h3 className='text-center' style={{ color: "#866a80" }}>Perfil de GitHub <FaBeer /></h3>
-                        <div className='text-center mt-3'>
-                        </div>
+                        <h1 className='text-center' style={{ color: "#866a80" }}>EduSys</h1>                   
+                        <p className='text-center col-6 mx-auto my-5'>Un lugar donde gestionar la información de tus estudiantes es mas fácil que nunca.</p>
+                    <div className='text-center mt-3'>
+                        <button className='col-md-4 btn text-white rounded-4 ' style={{ background: "#4d3147 " }}>Enviar <UilArrowRight/></button>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -41,4 +39,4 @@ function InicioHome() {
     )
 }
 
-export default InicioHome
+export default SomosNosotros

f838b590847b26d6fbc26a968f1ccbbb8b4ea3f1
TaniaSantucho <tssantucho@gmail.com>
2023-08-12 05:49:20 -0300

    acomodar tarjetas

--- src/pages/somosnosotros.jsx
+++ src/pages/somosnosotros.jsx
@@ -161,6 +161,37 @@ function InicioHome() {
                     </div>
                 </div>
             </div>
+            <div className="container row mx-auto py-lg-5 flex-md-row-reverse " >
+                <div className="col-lg-6 my-4 my-lg-0">
+                    <figure className='w-75 h-100 rounded-circle mx-auto overflow-hidden'
+                        style={{
+                            background: '#c9b7c7',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+
+                        }}>
+                        <img className='img-fluid'
+                            src={Conejo}
+                            alt="Imagen del usuario"
+                            style={{
+                                width: '90%',
+                            }}
+                        />
+                    </figure>
+
+                </div>
+                <div className="col-lg-6 mx-auto">
+                    <div >
+                        <h1 className='text-center' style={{ color: "#866a80" }}>NombreDesarrollador</h1>
+                        <p className='text-center col-6 mx-auto my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta quod cum eos molestiae, eligendi est eveniet odio tenetur odit dolor asperiores totam voluptatibus qui. Exercitationem, dolorem. Inventore, dolorem doloribus!</p>
+                        <h3 className='text-center' style={{ color: "#866a80" }}>Perfil de Linkedin <FaBeer />?</h3>
+                        <h3 className='text-center' style={{ color: "#866a80" }}>Perfil de GitHub <FaBeer /></h3>
+                        <div className='text-center mt-3'>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
